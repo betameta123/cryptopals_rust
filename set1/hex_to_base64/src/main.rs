5140deee13e875395be500923877785e82935cc8
betameta123 <kylehuang10@gmail.com>
2021-02-23 21:11:05 -0800

    hex_to_base

--- set1/hex_to_base64/src/main.rs
+++ set1/hex_to_base64/src/main.rs
@@ -1,3 +1,13 @@
+extern crate hex;
+extern crate base64;
 fn main() {
-    println!("Hello, world!");
+    let plain = match hex::decode("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d") {
+       Ok(v) => v,
+       Err(e) => {
+            eprintln!("error: {}", e);
+            vec![0,0,0]
+
+       },
+    };
+    println!("{:?}",base64::encode(plain));
 }
